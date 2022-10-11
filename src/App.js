01d0a5af75b639d7dd98f35ed831336b2aa04b02
jaguar017 <jayesh.singh2019@vitstudent.ac.in>
2022-10-12 01:57:55 +0530

    added search

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import FileUpload from './FileUpload/FileUpload.jsx';
 import FileList from './FileList/FileList';
+import FileSearch from "./FileSearch/FileSearch.jsx"
 function App() {
   const [files,setFiles]= useState([])
 
@@ -9,11 +10,13 @@ function App() {
 
     setFiles(files.filter(file => file.name !== filename))
   }
+  console.log(files)
   return (
     <div className="App">
       <p className='title'>Upload File</p>
       <h1> hi hii Hello </h1>
       <FileUpload files={files} setFiles={setFiles} removeFile={removeFile}/>
+      <FileSearch files={files} setFiles={setFiles}/>
       <FileList files={files} removeFiles={removeFile} />
     </div>
   );
